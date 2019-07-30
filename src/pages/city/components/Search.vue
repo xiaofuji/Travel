1bf299633070d5b4c773d383ec4a836e330eccf5
xiaofuji <f13263352898@163.com>
2019-07-30 12:01:37 +0800

    opti search city

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,19 +1,82 @@
 <template>
-  <div class="search">
-    <input
-      type="text"
-      class="search-input"
-      placeholder="输入城市名或拼音"
+  <div>
+    <div class="search">
+      <input
+        v-model='keyword'
+        type="text"
+        class="search-input"
+        placeholder="输入城市名或拼音"
+      >
+    </div>
+    <div
+      class="search-content"
+      ref='search'
+      v-show="keyword"
     >
+      <ul>
+        <li
+          class='search-item'
+          v-for="item of list"
+          :key='item.id'
+        >
+          {{item.name}}
+        </li>
+        <li
+          class='search-item'
+          v-show="hasNoData"
+        >
+          没有找到匹配数据
+        </li>
+      </ul>
+    </div>
   </div>
 
 </template>
 
 <script>
-
+import BScroll from 'better-scroll'
 export default {
   name: 'CitySearch',
-
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -37,6 +100,25 @@ export default {
     font-size: 0.14rem;
     border: none;
     border-radius: 0.03rem;
+  }
+}
+
+.search-content {
+  z-index: 1;
+  overflow: hidden;
+  position: absolute;
+  top: 0.7rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: #eee;
+
+  .search-item {
+    padding-left: 0.1rem;
+    line-height: 0.31rem;
+    background: #fff;
+    font-size: 0.16rem;
+    color: #666;
   }
 }
 </style>
